dd2eaaca002eb57f01dce0421f4ca57ce1ef9787
georgegamelr-spec <georgegamelr@gmail.com>
2026-04-22 15:26:40 +0200

    fix: Remove deprecated swcMinify option (default in Next.js 14)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify is enabled by default in Next.js 14+, no need to specify
   images: {
     remotePatterns: [
       {
